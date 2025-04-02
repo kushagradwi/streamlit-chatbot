a7512d283bad9547539cfa22865fb227050fb017
punith jain <punith.jain1@gmail.com>
2025-04-03 01:09:07 +0530

    login css fix

--- login.py
+++ login.py
@@ -16,12 +16,11 @@ def get_base64_font(path):
     with open(path, "rb") as font_file:
         return base64.b64encode(font_file.read()).decode("utf-8")
 
-st.set_page_config(page_title="Review Insights ChatBOT - Login", page_icon="🤖", layout="centered")
+st.set_page_config(page_title="Review Insights ChatBOT - Login", page_icon="🤖", layout="wide")
 
-logo_base64 = get_base64_image("static/login/microsoft.png")
-background_base64 = get_base64_image("static/login/Background.png")
-vystar_logo_base64 = get_base64_image("static/login/Vystar-logo.png")
-
+with open("static/css/login.css") as login_css_file:
+    st.html(f"<style>{login_css_file.read()}</style>")
+    
 poppins_light_base64 = get_base64_font("assets/fonts/Poppins/Poppins-Light.ttf")
 poppins_regular_base64 = get_base64_font("assets/fonts/Poppins/Poppins-Regular.ttf")
 poppins_medium_base64 = get_base64_font("assets/fonts/Poppins/Poppins-Medium.ttf")
@@ -31,141 +30,8 @@ poppins_extralight_base64 = get_base64_font("assets/fonts/Poppins/Poppins-ExtraL
 
 st.markdown(
     f"""
-    <style>
-        @font-face {{
-            font-family: 'Poppins';
-            font-style: normal;
-            font-weight: 300;
-            src: url(data:font/ttf;base64,{poppins_light_base64}) format('truetype');
-        }}
-        @font-face {{
-            font-family: 'Poppins';
-            font-style: normal;
-            font-weight: 200;
-            src: url(data:font/ttf;base64,{poppins_extralight_base64}) format('truetype');
-        }}
-
-        @font-face {{
-            font-family: 'Poppins';
-            font-style: normal;
-            font-weight: 400;
-            src: url(data:font/ttf;base64,{poppins_regular_base64}) format('truetype');
-        }}
-        @font-face {{
-            font-family: 'Poppins';
-            font-style: normal;
-            font-weight: 500;
-            src: url(data:font/ttf;base64,{poppins_medium_base64}) format('truetype');
-        }}
-        @font-face {{
-            font-family: 'Poppins';
-            font-style: normal;
-            font-weight: 600;
-            src: url(data:font/ttf;base64,{poppins_semibold_base64}) format('truetype');
-        }}
-        @font-face {{
-            font-family: 'Poppins';
-            font-style: normal;
-            font-weight: 700;
-            src: url(data:font/ttf;base64,{poppins_bold_base64}) format('truetype');
-        }}
-
-        body, html, .stApp {{
-            font-family: 'Poppins', sans-serif;
-            background: url(data:image/png;base64,{background_base64}) no-repeat center center fixed;
-            background-size:cover;
-            margin: 0;
-            padding: 0;
-            box-sizing: border-box;
-            color: white;
-        }}
-
-        .login-title-container {{
-            position: fixed;
-            left: 181px;
-            bottom: 411px;
-        }}
-
-        .login-title {{
-            font-family: 'Poppins', sans-serif;
-            font-weight: 300;
-            font-size: 36px;
-            line-height: 54px;
-            color: white;
-            margin-bottom: 8px;
-            text-align: center;
-        }}
-
-        .login-subtitle-container {{
-            position: fixed;
-            left: 182px;
-            bottom: 351px;
-        }}
-
-        .login-subtitle {{
-            font-family: 'Poppins', sans-serif;
-            font-size: 16px;
-            font-weight: 200;
-            line-height: 24px;
-            color: white;
-        }}
-        .footer{{
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        }}
-        .footer-text{{
-            // © 2025 Vystar, Incorporated and its Affiliates. All Rights Reserved
-            color: white;
-            font-size: 10px;
-            font-family: Poppins;
-            font-weight: 400;
-            word-wrap: break-word
-        }}
-        div[data-testid="stButton"] {{
-            position: fixed;
-            left: 168px;
-            bottom: 280px;
-        }}
-
-        div[data-testid="stButton"] > button {{
-            background-color: #006EF5 !important;
-            color: white;
-            font-size: 16px;
-            font-weight: 600;
-            border-radius: 5px;
-            border: none;
-            padding: 12px;
-            width: 300px;
-            height: 48px;
-            display: flex;
-            align-items: center;
-            justify-content: center;
-        }}
-
-        .vystar-logo-container {{
-            position: fixed;
-            left: 243px;
-            bottom: 553px;
-        }}
-
-        .vystar-logo-container img {{
-            width: 150px; /* Adjust size as needed */
-        }}
-
-        .footer {{
-            position: fixed;
-            bottom: 0;
-            left: 0;
-            width: 100vw;
-            height: 62px;
-            background: #006EF5;
-        }}
-
-        header {{visibility: hidden;}}
-    </style>
     <div class="vystar-logo-container">
-        <img src="data:image/png;base64,{vystar_logo_base64}" alt="VyStar Logo">
+        <img src="app/static/login/Vystar-logo.png" alt="VyStar Logo">
     </div>
     """,
     unsafe_allow_html=True,
@@ -179,8 +45,8 @@ if not st.session_state["authenticated"]:
     st.markdown('<div class="login-subtitle-container"><div class="login-subtitle">Sign in using Microsoft credentials</div></div>', unsafe_allow_html=True)
 
     if st.button("SIGN IN", key="microsoft-sign-in"):
-        #mock_microsoft_sso()
-        azure_sso_handler()
+        mock_microsoft_sso()
+        # azure_sso_handler()
 
     st.markdown('<div class="footer"><div class= "footer-text" style="width: 381.65px; text-align: center; color: white; font-size: 10px; font-family: Poppins; font-weight: 400; word-wrap: break-word">© 2025 Vystar, Incorporated and its Affiliates. All Rights Reserved</div></div>', unsafe_allow_html=True)
 
